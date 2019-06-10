0da63b0f8a7a022897e25ba1d63b31d266bdce62
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2019-06-10 13:23:25 +1000

    getting all article titles from the first page

--- scrapy_theguardian/scrapy_theguardian/spiders/articles_spider.py
+++ scrapy_theguardian/scrapy_theguardian/spiders/articles_spider.py
@@ -9,4 +9,10 @@ class ArticlesSpider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        for article_link in response.css('a[data-link-name="article"]::attr(href)').getall():
+            yield scrapy.Request(url=article_link, callback=self.parse_article_content)
+    
+    def parse_article_content(self, response):
+        yield {
+            "headline": response.css('h1[itemprop="headline"]::text').get()
+        }
--- scrapy_theguardian/tests/test_parse_articles.py
+++ scrapy_theguardian/tests/test_parse_articles.py
@@ -1,11 +1,27 @@
 import unittest
+from scrapy.http import HtmlResponse
 from scrapy_theguardian.spiders.articles_spider import ArticlesSpider
 
 class TestArticlesSpider(unittest.TestCase):
 
-    def test_pares_page(self):
+    def test_parse_page(self):
         articles_spider = ArticlesSpider()
-        self.assertEqual(articles_spider.parse(None), None)
+        res = HtmlResponse(url='', encoding='utf-8', body='<html><body><a href="http://test1.com" data-link-name="article">Test1</a>\
+        <a href="http://test2.com" data-link-name="ads">Test2 ads</a>\
+        <a href="http://test3.com" data-link-name="article">Test3</a></body></html>')
+        ret = [x for x in articles_spider.parse(res)]
+        self.assertEqual(ret.__str__(), '[<GET http://test1.com>, <GET http://test3.com>]')
+
+    def test_parse_article(self):
+        articles_spider = ArticlesSpider()
+        res = HtmlResponse(url='', encoding='utf-8', body='<html><body><h1 class="content__headline content__headline--no-margin-bottom" itemprop="headline">\
+Title Parsed\
+</h1></body></html>')
+        ret = [x for x in articles_spider.parse_article_content(res)]
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0], {
+            'headline': 'Title Parsed',
+        })
 
 if __name__ == '__main__':
     unittest.main()
