376ca2d774af8fe3d6a4022ce1ec5a44eab0063b
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2019-06-10 15:28:49 +1000

    getting keywords

--- scrapy_theguardian/scrapy_theguardian/spiders/articles_spider.py
+++ scrapy_theguardian/scrapy_theguardian/spiders/articles_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 
+MAX_PAGE_REQUESTS = 2
 
 class ArticlesSpider(scrapy.Spider):
     name = "articles"
@@ -11,15 +12,22 @@ class ArticlesSpider(scrapy.Spider):
     def parse(self, response):
         for article_link in response.css('a[data-link-name="article"]::attr(href)').getall():
             yield scrapy.Request(url=article_link, callback=self.parse_article_content)
+        next_page_number = int(response.css('a[data-link-name="Pagination view next"]::attr(data-page)').get())
+        print(next_page_number)
+        if next_page_number <= MAX_PAGE_REQUESTS:
+            next_page_url = response.css('a[data-link-name="Pagination view next"]::attr(href)').get()
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+
     
     def parse_article_body(self, selector):
         return '\n'.join(["".join(x.css("::text").getall()).strip() for x in selector])
 
     def parse_article_content(self, response):
         yield {
-            'headline': response.css('h1[itemprop="headline"]::text').get().strip(),
-            'description': response.css('meta[itemprop="description"]::attr(content)').get().strip(),
+            'headline': (response.css('h1[itemprop="headline"]::text').get() or '').strip(),
+            'description': (response.css('meta[itemprop="description"]::attr(content)').get() or '').strip(),
             'body': self.parse_article_body(response.css('div[itemprop="articleBody"] > p')),
             'url': response.url,
-            'author': response.css('span[itemprop="author"] span[itemprop="name"]::text').get().strip(),
+            'author': (response.css('span[itemprop="author"] span[itemprop="name"]::text').get() or '').strip(),
+            'keywords': (response.css('meta[name="keywords"]::attr(content)').get() or '').split(","),
         }
--- scrapy_theguardian/tests/test_parse_articles.py
+++ scrapy_theguardian/tests/test_parse_articles.py
@@ -9,14 +9,24 @@ class TestArticlesSpider(unittest.TestCase):
         articles_spider = ArticlesSpider()
         res = HtmlResponse(url='', encoding='utf-8', body='<html><body><a href="http://test1.com" data-link-name="article">Test1</a>\
         <a href="http://test2.com" data-link-name="ads">Test2 ads</a>\
-        <a href="http://test3.com" data-link-name="article">Test3</a></body></html>')
+        <a href="http://test3.com" data-link-name="article">Test3</a>\
+        <a data-page="2" rel="next" href="https://test.com/?page=2" data-link-name="Pagination view next"></a></body></html>')
+        ret = [x for x in articles_spider.parse(res)]
+        self.assertEqual(ret.__str__(), '[<GET http://test1.com>, <GET http://test3.com>, <GET https://test.com/?page=2>]')
+    
+    def test_parse_last_page(self):
+        articles_spider = ArticlesSpider()
+        res = HtmlResponse(url='', encoding='utf-8', body='<html><body><a href="http://test1.com" data-link-name="article">Test1</a>\
+        <a href="http://test2.com" data-link-name="ads">Test2 ads</a>\
+        <a href="http://test3.com" data-link-name="article">Test3</a>\
+        <a data-page="6" rel="next" href="https://test.com/?page=2" data-link-name="Pagination view next"></a></body></html>')
         ret = [x for x in articles_spider.parse(res)]
         self.assertEqual(ret.__str__(), '[<GET http://test1.com>, <GET http://test3.com>]')
 
     def test_parse_article(self):
         articles_spider = ArticlesSpider()
         url = 'http://testurl.com'
-        res = HtmlResponse(url=url, encoding='utf-8', body='<html><body>\
+        res = HtmlResponse(url=url, encoding='utf-8', body='<html><body><header><meta name="keywords" content="a,b,c"></header>\
             <h1 class="content__headline content__headline--no-margin-bottom" itemprop="headline">\
             Title Parsed\
             </h1><span itemprop="author"> \
@@ -31,6 +41,27 @@ class TestArticlesSpider(unittest.TestCase):
             'body': 'body 1\nbody 2',
             'url': url,
             'author': 'Author Name',
+            'keywords': ['a','b','c']
+        })
+
+    def test_parse_article_no_description(self):
+        articles_spider = ArticlesSpider()
+        url = 'http://testurl.com'
+        res = HtmlResponse(url=url, encoding='utf-8', body='<html><body>\
+            <h1 class="content__headline content__headline--no-margin-bottom" itemprop="headline">\
+            Title Parsed\
+            </h1><span itemprop="author"> \
+            <a><span itemprop="name">Author Name</span></a></span>\
+            <div itemprop="articleBody"><p>body 1</p><p>body 2</p></div></body></html>')
+        ret = [x for x in articles_spider.parse_article_content(res)]
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0], {
+            'headline': 'Title Parsed',
+            'description': '',
+            'body': 'body 1\nbody 2',
+            'url': url,
+            'author': 'Author Name',
+            'keywords' : ['']
         })
     
     def test_parse_article_body(self):
